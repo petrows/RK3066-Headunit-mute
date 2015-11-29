ddcc57f414f54023a39e33d4c556bc292ce2daa8
Petro <petro@petro.ws>
2015-11-29 11:59:30 +0300

    init commit

---
+++ app/src/main/java/ws/petro/mtcheadunit/mute/MainActivity.java
@@ -0,0 +1,66 @@
+package ws.petro.mtcheadunit.mute;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AudioManager am = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        am = ((AudioManager) getSystemService(Context.AUDIO_SERVICE));
+
+        boolean currentMute = getMute();
+        boolean newMute = !currentMute;
+
+        Toast.makeText(this, "Set Mute: " + (newMute?"TRUE":"FALSE"), Toast.LENGTH_SHORT).show();
+        setmute(newMute); // Inverse mute
+
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean getMute() {
+        return am.getParameters("av_mute=").equals("true");
+    }
+
+    public void setmute(boolean value) {
+        am.setParameters("av_mute=" + value);
+    }
+}
