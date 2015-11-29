6aef218a48a6245940357adbff647ab9f1c7959a
Petro <petro@petro.ws>
2015-11-29 17:49:38 +0300

    fix view

--- app/src/main/java/ws/petro/mtcheadunit/mute/MainActivity.java
+++ app/src/main/java/ws/petro/mtcheadunit/mute/MainActivity.java
@@ -1,5 +1,6 @@
 package ws.petro.mtcheadunit.mute;
 
+import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.os.Bundle;
@@ -12,16 +13,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private AudioManager am = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         am = ((AudioManager) getSystemService(Context.AUDIO_SERVICE));
 
@@ -32,28 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setmute(newMute); // Inverse mute
 
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public boolean getMute() {
